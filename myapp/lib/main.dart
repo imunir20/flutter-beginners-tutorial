e8c6c3874373245cb61cca39286d34acb23add6f
imunir20 <ibrahimmunir13@gmail.com>
2023-12-24 09:23:16 -0500

    Basic UI lessons

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -2,8 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text('Wassup brothers.'),
+    home: Home(),
   ));
 }
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[400],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(90),
+        child: Text('hello')
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.red[400],
+      ),
+    );
+  }
+}
+
+
 
 
