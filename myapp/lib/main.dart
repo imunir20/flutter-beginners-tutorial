2be972252c90cf72f16ec3aa667c781784a051a5
imunir20 <ibrahimmunir13@gmail.com>
2023-12-24 10:14:26 -0500

    Rows, Columns, shortcuts, etc.

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -15,9 +15,37 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[400],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(90),
-        child: Text('hello')
+      body: Row(
+        children: <Widget> [
+          Expanded(
+            child: Image.asset('assets/colts-flutter-test-img.webp'),
+            flex: 3
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.green[400],
+              child: Text('1'),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.blue[300],
+              child: Text('2'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.purple[300],
+              child: Text('3'),
+            ),
+          ),
+        ]
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
